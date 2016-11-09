c9f646efc60618bf971e3e12e49547ca4b4d4060
zhuzhugo <709967921@qq.com>
2016-11-09 16:52:01 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zmy1/hday13_zzz/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zmy1.hday13_zzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
