b4eca797e52d3e0fea9ae0ac388abe89f6e68538
zhuzhugo <709967921@qq.com>
2016-11-10 10:16:33 +0800

    这是朱梦雨的分之

---
+++ app/src/main/java/com/example/zmy1/hday13_zzz/TwoActivity.java
@@ -0,0 +1,15 @@
+package com.example.zmy1.hday13_zzz;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zmy1 on 2016/11/10.
+ */
+public class TwoActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
